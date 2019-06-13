5f80a5475963cad341656a66e62611412ce57837
Witzelsucht <kszalaty00@gmail.com>
2019-06-13 14:30:06 +0200

    Alpha 0.8a

--- client/scripts/lobby.js
+++ client/scripts/lobby.js
@@ -2,140 +2,120 @@ var socket = io.connect('http://localhost:2137');
 var nick;
 var clientHost;
 
-socket.on('getNickname', function(message, randomnick){
-    do
-    {
+socket.on('getNickname', function (message, randomnick) {
+    do {
         nick = prompt(message, randomnick);
-    }while(nick == null);
+    } while (nick == null);
     socket.emit('resNickname', nick);
 });
 
-socket.on('entry', function(data, host){
+socket.on('FullLobby', function(){
+    alert("Lobby jest pełne");
+    window.location.href = "http://www.google.com";
+});
+
+socket.on('entry', function (data, host) {
     var connected = document.getElementById('connected');
     clientHost = host;
     connected.innerHTML = "";
-    for(var i = 0; i<data.length; i++)
-    {
-        if(clientHost == nick)
-        {
-            if(nick == data[i])
-            {
+    for (var i = 0; i < data.length; i++) {
+        if (clientHost == nick) {
+            if (nick == data[i]) {
                 connected.innerHTML += '<p class="localclient"><strong>' + data[i] + '</strong></p>';
             }
-            else
-            {
-                connected.innerHTML += '<p class="client"><strong>' + data[i] + '</strong><button onclick=wypierdol('+ i +')>Kick</button><button onclick="promote(' + i + ')">Promote</button></p>';
+            else {
+                connected.innerHTML += '<p class="client"><strong>' + data[i] + '</strong><button onclick=wypierdol(' + i + ')>Kick</button><button onclick="promote(' + i + ')">Promote</button></p>';
             }
         }
-        else
-        {
-            if(nick == data[i])
-            {
+        else {
+            if (nick == data[i]) {
                 connected.innerHTML += '<p class="localclient"><strong>' + data[i] + '</strong></p>';
             }
-            else
-            {
+            else {
                 connected.innerHTML += '<p class="client"><strong>' + data[i] + '</strong>';
             }
         }
     }
     document.getElementById('header').innerHTML = '<h2>Lobby host: ' + host + '</h2>';
     var nodes = document.getElementById("options").getElementsByTagName('*');
-    if(clientHost == nick)
-    {
-        for(var i = 0; i < nodes.length; i++)
-        {
+    if (clientHost == nick) {
+        for (var i = 0; i < nodes.length; i++) {
             nodes[i].disabled = false;
         }
         document.getElementById('start').disabled = false;
     }
-    else
-    {
-        for(var i = 0; i < nodes.length; i++)
-        {
+    else {
+        for (var i = 0; i < nodes.length; i++) {
             nodes[i].disabled = true;
         }
         document.getElementById('start').disabled = true;
     }
 });
 
-socket.on('start', function(){
+socket.on('start', function () {
     window.location.href = '/game';
 });
 
-function start(){
+function start() {
 
     socket.emit('start');
 }
 
-function enterChatSend(e)
-{
-    if(e.keyCode == 13)
-    {
+function enterChatSend(e) {
+    if (e.keyCode == 13) {
         chatSend();
     }
 }
 
-function wypierdol(i)
-{
+function wypierdol(i) {
     socket.emit('Kick', i);
 }
 
-socket.on('Kicked', function(data){
-    if(nick == data)
-    {
+socket.on('Kicked', function (data) {
+    if (nick == data) {
         alert("Zostałeś wyrzucony z pokoju");
         window.location.href = "http://www.google.com";
     }
 });
 
-function promote(i)
-{
+function promote(i) {
     socket.emit('Promote', i);
 }
 
-function sendOptions()
-{
+function sendOptions() {
     var maxPlayers = document.getElementById("maxPlayers").value;
     var monstersCheck = [];
     var monsters = document.getElementsByClassName("monster");
     var playedCards = [];
     var cards = document.getElementsByClassName("card");
-    for(var i = 0; i<cards.length; i++){
-        if(cards[i].checked==true)
-        {
+    for (var i = 0; i < cards.length; i++) {
+        if (cards[i].checked == true) {
             playedCards.push(cards[i].value);
         }
     };
-    for(var i = 0; i<monsters.length; i++){
-        if(monsters[i].checked == true)
-        {
+    for (var i = 0; i < monsters.length; i++) {
+        if (monsters[i].checked == true) {
             monstersCheck.push(monsters[i].value);
         }
     };
-    var data = {lobbyLimit: maxPlayers, cards: playedCards};
-    if(monstersCheck.length > 0)
-    {
+    var data = { lobbyLimit: maxPlayers, cards: playedCards };
+    if (monstersCheck.length > 0) {
         var cardNumber = parseInt(maxPlayers) + 3;
-        if(playedCards.length == cardNumber)
-        {
+        if (playedCards.length == cardNumber) {
             socket.broadcast.emit('Options', data);
         }
-        else
-        {
+        else {
             alert("Zaznaczyłeś nieprawidłową ilość kart");
         }
     }
-    else
-    {
+    else {
         alert("Nie zaznaczyłeś karty potwora");
     }
 }
 
-function chatSend(){
+function chatSend() {
     var wschk = (document.getElementById('message').value).trim();
-    if(wschk != "")
-    {
+    if (wschk != "") {
         socket.emit('chat', {
             sender: nick,
             message: document.getElementById('message').value,
@@ -144,17 +124,15 @@ function chatSend(){
     }
 }
 
-socket.on('AYST', function(){
+socket.on('AYST', function () {
     socket.emit('AYSTResponse', nick);
 })
 
-socket.on('chat', function(data){
-    if(data.sender == nick)
-    {
+socket.on('chat', function (data) {
+    if (data.sender == nick) {
         document.getElementById('output').innerHTML += '<p style="margin-left: 10px;"><strong class="localclient">' + data.sender + ': </strong>' + data.message + '</p>';
     }
-    else
-    {
+    else {
         document.getElementById('output').innerHTML += '<p style="margin-left: 10px;"><strong class="client">' + data.sender + ': </strong>' + data.message + '</p>';
     }
 });
--- controllers/LobbyController.js
+++ controllers/LobbyController.js
@@ -1,66 +1,66 @@
 var socket = require('socket.io');
 var allClients = [];
-var gameRules;
+var gameRules = { lobbyLimit: 5, cards: null };
 var host;
 
-function randomNick(){
+function randomNick() {
     var nickBase = ["Nowakus", "Duża Roxi", "Huberinio", "Wrotka Wywrotka", "Twój Stary", "Twoja Stara", "Dzban"];
-    var nick = nickBase[Math.floor(Math.random()*7)];
+    var nick = nickBase[Math.floor(Math.random() * 7)];
     return nick;
 }
 
-module.exports = function(app, server){
+module.exports = function (app, server) {
     io = socket(server);
-    io.on('connection', function(socket){
-        if(allClients[0] == null)
-        {
-            host = "";
+    io.on('connection', function (socket) {
+        if (allClients.length >= gameRules.lobbyLimit) {
+            socket.emit('FullLobby');
         }
-        socket.emit('entry', allClients, host);
-        socket.emit('getNickname', "Podaj nick" ,randomNick());
-        socket.on('resNickname', function(data){
-            var nickTaken = false;
-            for(var i = 0; i<allClients.length; i++)
-            {
-                if(allClients[i] == data)
-                {
-                    nickTaken = true;
+        else {
+            if (allClients[0] == null) {
+                host = "";
+            }
+            socket.emit('entry', allClients, host);
+            socket.emit('getNickname', "Podaj nick", randomNick());
+            socket.on('resNickname', function (data) {
+                var nickTaken = false;
+                for (var i = 0; i < allClients.length; i++) {
+                    if (allClients[i] == data) {
+                        nickTaken = true;
+                    }
                 }
-            }
-            if(nickTaken)
-            {
-                socket.emit('getNickname', "Nick już zajęty", randomNick());
-            }
-            else
-            {
-                allClients.push(data);
+                if (nickTaken) {
+                    socket.emit('getNickname', "Nick już zajęty", randomNick());
+                }
+                else {
+                    allClients.push(data);
+                    host = allClients[0];
+                    io.sockets.emit('entry', allClients, host);
+                }
+            });
+            socket.on('disconnect', function (socket) {
+                io.sockets.emit('AYST');
+                allClients = [];
+            });
+            socket.on('AYSTResponse', function (res) {
+                allClients.push(res);
                 host = allClients[0];
-                io.sockets.emit('entry', allClients, host);
-            }
-        });
-        socket.on('disconnect', function(socket){
-            io.sockets.emit('AYST');
-            allClients = [];
-        });
-        socket.on('AYSTResponse', function(res){
-            allClients.push(res);
-            host = allClients[0];
-            io.sockets.emit('entry', allClients, host)
-        });
-        socket.on('start', function(){
-            io.sockets.emit('start');
-        })  
-        socket.on('chat', function(data){
-            io.sockets.emit('chat', data);
-        });
-        socket.on('Options', function(data){
-            gameRules = data;
-        });
-        socket.on('Promote', function(data){
-            io.sockets.emit('entry', allClients, allClients[data])
-        });
-        socket.on('Kick', function(data){
-            io.sockets.emit('Kicked', allClients[data]);
-        });
+                io.sockets.emit('entry', allClients, host)
+            });
+            socket.on('start', function () {
+                io.sockets.emit('start');
+            })
+            socket.on('chat', function (data) {
+                io.sockets.emit('chat', data);
+            });
+            socket.on('Options', function (data) {
+                gameRules = data;
+            });
+            socket.on('Promote', function (data) {
+                io.sockets.emit('entry', allClients, allClients[data])
+            });
+            socket.on('Kick', function (data) {
+                io.sockets.emit('Kicked', allClients[data]);
+            });
+        }
     });
 }
